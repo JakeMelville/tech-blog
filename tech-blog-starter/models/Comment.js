91d7814422a281f9144e3860cc7aabdde14ee30b
Jake Melville <jakemelville@ymail.com>
2021-03-18 19:06:49 -0400

    imported starter code, created models for post and comments

---
+++ tech-blog-starter/models/Comment.js
@@ -0,0 +1,44 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+const { init } = require('./User');
+
+// TODO: YOUR CODE HERE
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User'
+    }
+)
+
+module.exports = Comment;
---
+++ tech-blog-starter/models/Post.js
@@ -0,0 +1,39 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+// TODO: YOUR CODE HERE
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User'
+    });
+
+module.exports = Post;
