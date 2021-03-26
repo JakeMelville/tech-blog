25dc56e469b4e9d8c45aab80145108f16bba60ac
Jake Melville <jakemelville@ymail.com>
2021-03-26 14:51:07 -0400

    updated .env file to macth jaws

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,13 +4,20 @@ const withAuth = require('../../utils/auth');
 
 // URL: 
 router.post('/', withAuth, async (req, res) => {
+  console.log('/api/comment hits');
   try {
     const newComment = await Comment.create({
       // TODO: COMMENT BODY IN REQUEST USING SPREAD
 
       // TODO: SET USERID TO SESSION LOGGEDIN USERID
+      title: req.body.title,
+      content: req.body.body,
+      userId: req.session.userId
+
+
     });
     res.json(newComment);
+    console.log(" line 20 new comment", newComment);
   } catch (err) {
     res.status(500).json(err);
   }
