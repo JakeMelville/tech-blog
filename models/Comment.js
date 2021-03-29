2e9f5c6829e449869678888c31c2d12f6c7c32c3
Jake Melville <jakemelville@ymail.com>
2021-03-29 16:07:28 -0400

    updated models

--- models/Comment.js
+++ models/Comment.js
@@ -35,7 +35,7 @@ Comment.init(
     {
         sequelize,
         // timestamps: false,
-        freezeTableName: true,
+        // freezeTableName: true,
         // underscored: true,
         // modelName: 'User'
     }
